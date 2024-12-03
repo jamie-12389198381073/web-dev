5760e76e7a3d2e68856e55201a0c4ccc85609fa4
Alcodes-tech <Alanbishwakarma@gmail.com>
2024-12-03 12:04:13 +0000

    added to js and json file

--- contact.js
+++ contact.js
@@ -10,3 +10,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
      })
     })
+
+    myForm.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        const formBody = {
+            name:myName.value,
+            email:myEmail.value,
+            message:myMessage.value
+            };
+        const requestHeaders = {
+            "Content-Type": "application/json"
+        };
+        fetch("/contact", {
+            method: 'POST',
+            headers: requestHeaders,
+            body: JSON.stringify(formBody)
+            }).then(response => response.json())
+            .then (responsedata =>{ console.log(responsedata);
+                myConfirmation.textContent=`Hi ${responsedata.name}, your message has
+                been received, we will contact you at ${responsedata.email}`;
+        });
+    });
