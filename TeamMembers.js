f49b21397394094ce38aa55f925b7949da2a4526
jamie-12389198381073 <jamiec100q@gmail.com>
2024-12-05 20:22:15 +0000

    started to do my java scrpit properly

---
+++ TeamMembers.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.title = "Team Members - Sustainbility";
+    //const header = document.getElementById("main-header");
+    let header = document.getElementById("teamMembers");
+    header.textContent = "team members";
+});
