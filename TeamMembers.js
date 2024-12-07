4fb18f2e93f9c9a383a33f36428ccc63b1a2d413
jamie-12389198381073 <jamiec100q@gmail.com>
2024-12-07 05:22:56 +0000

    doing more team members

--- TeamMembers.js
+++ TeamMembers.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.title = "Team Members - Sustainbility";
-    //const header = document.getElementById("main-header");
     let header = document.getElementById("teamMembers");
     header.textContent = "team members";
+
+    let mypara1 = document.getElementById("mypara1");
+    mypara1.textContent = "This page will showcase our amazing team members with infomration about what they do and why they want to do it. It will also tell you about why they chose to work in this industy and more genral information about them";
+    
+
+
 });
+
 
