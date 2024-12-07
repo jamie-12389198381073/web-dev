872eefeaf1ad70afac28ef5c3aa04b73705dc182
JaMalak23 <kubamalak02@gmail.com>
2024-12-07 21:00:54 +0000

    made nav and footer more consistent throughout the pages

--- TeamMembers.js
+++ TeamMembers.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.title = "Team Members - Sustainbility";
     let header = document.getElementById("teamMembers");
-    header.textContent = "team members";
+    header.textContent = "Team Members";
 
     let mypara1 = document.getElementById("mypara1");
     mypara1.textContent = "This page will showcase our amazing team members with infomration about what they do and why they want to do it. It will also tell you about why they chose to work in this industy and more genral information about them";
