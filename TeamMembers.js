147784db28610970458d7b0135d8e3c551217901
jamie-12389198381073 <jamiec100q@gmail.com>
2024-12-07 17:05:55 +0000

    yh
    yh

--- TeamMembers.js
+++ TeamMembers.js
@@ -6,8 +6,49 @@ document.addEventListener("DOMContentLoaded", () => {
     let mypara1 = document.getElementById("mypara1");
     mypara1.textContent = "This page will showcase our amazing team members with infomration about what they do and why they want to do it. It will also tell you about why they chose to work in this industy and more genral information about them";
     
-
-
 });
 
+
+if (sectionElement = document.querySelector("#section")) {
+    let localJsonFile = "Team.json";
+    document.addEventListener("DOMContentLoaded", () => {
+      fetch(localJsonFile)
+        .then((response) => response.json())
+        .then((responseData) => {
+          for (item of responseData) {
+            const schedule = document.createElement("article");
+            const imageElement = document.createElement("img");
+            sectionElement.appendChild(schedule);
+            imageElement.src = item.imageURL;
+            imageElement.alt = item.alt;
+    
+            imageElement.classList.add("small-image");
+    
+            schedule.appendChild(imageElement);
+            const headElement = document.createElement("h4");
+            const paraElement1 = document.createElement("p");
+            const paraElement2 = document.createElement("p");
+            const paraElement3 = document.createElement("p");
+            const paraElement4 = document.createElement("p");
+    
+            schedule.setAttribute("class", "servies");
+            schedule.appendChild(headElement);
+            headElement.textContent = "Yoga style: " + item.style;
+    
+            schedule.appendChild(paraElement1);
+            paraElement1.textContent = "time: " + item.time;
+    
+            schedule.appendChild(paraElement2);
+            paraElement2.textContent = "focus: " + item.focus;
+    
+            schedule.appendChild(paraElement3);
+            paraElement3.textContent = "benefits: " + item.benefits;
+    
+            schedule.appendChild(paraElement4);
+            paraElement4.textContent = "level: " + item.level;
+          }
+        })
+        .catch((error) => console.error("Error fetching JSON data", error));
+    });
+    }
 
