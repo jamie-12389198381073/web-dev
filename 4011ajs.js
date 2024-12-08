d0df706cc1a08196a0ba50f01eec8d15485bab96
theonerishi <135510283+theonerishi@users.noreply.github.com>
2024-12-08 12:44:26 +0000

    trying to merge branches

---
+++ 4011ajs.js
@@ -0,0 +1,1 @@
+let localJsonFile = 'goals.json';
--- express.js
+++ express.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('/'));
 
-document.addEventListener('DOMContentLoaded'), () => {
-    fetch()
-}
+app.get("/goals.html", (req,res) => {
+     fs.readFile(goals.json, 'utf8', (err, data) => {
+        console.log(data);
+        let h1 = document.createElement('h1');
+        let h2 = document.createElement('h1');
+        let h3 = document.createElement('h1');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let div1 = document.querySelector('#div1');
+        let div2 = document.querySelector('#div2');
+        let div3 = document.querySelector('#div3');
+        div1.appendChild(h1);
+        div1.appendChild(p1);
+        div2.appendChild(h2);
+        div2.appendChild(p2);
+        div3.appendChild(h3);
+        div3.appendChild(p3);
+        h1.textContent = "Industry";
+        p1.textContent = "Industry text";
+        h2.textContent = "Innovation and Infrastructure";
+        p2.textContent = "Innovation and Infrastructure text";
+        h3.textContent = "No Poverty";
+        p3.textContent= "No Poverty text";
+     })
+     
+ })
